6c01043e50f4d6afd678e1295438437e4a007887
luchnikovs <harvey2009@yandex.ru>
2020-06-23 09:42:16 +0300

    Excess was cut out

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import home from './home'
-// import history from 'history'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    home,
-    // history
+    home
   }
 })
